2da876b4bc73810bcb7123f943f1b559d9bec7a8
gh28942 <GerH@example.com>
2021-03-07 01:39:46 +0100

    Updates

---
+++ precache-manifest.8581d335e21ef0f6d553cf60e2e7b087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f574375b8cee4cbb3fbd8f9e55d03e5c",
+    "url": "/powerball-simulator/index.html"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/powerball-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "3df565858cb9ae5ffaad",
+    "url": "/powerball-simulator/static/css/main.4f598a1a.chunk.css"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/powerball-simulator/static/js/2.effe3d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/powerball-simulator/static/js/2.effe3d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df565858cb9ae5ffaad",
+    "url": "/powerball-simulator/static/js/main.a049821d.chunk.js"
+  },
+  {
+    "revision": "83b993abc618b3a67001",
+    "url": "/powerball-simulator/static/js/runtime-main.dbb76205.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/powerball-simulator/precache-manifest.8581d335e21ef0f6d553cf60e2e7b087.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/powerball-simulator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
